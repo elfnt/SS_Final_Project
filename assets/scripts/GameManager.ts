ad4d7ee1255e880d2fafd35a43a2925d41895122
AndyLee <andylee00012@gmail.com>
2025-05-24 23:51:52 +0800

    Updated animation

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -13,8 +13,6 @@ export default class GameManager extends cc.Component {
             cc.PhysicsManager.DrawBits.e_aabbBit |
             cc.PhysicsManager.DrawBits.e_jointBit |
             cc.PhysicsManager.DrawBits.e_shapeBit;
-        //physicsMgr.debugDrawFlags = 0;
-        physicsMgr.gravity = cc.v2(0, -1600);
     }
 
     start() {
